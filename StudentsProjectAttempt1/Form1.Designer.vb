c5da8cd92c016d9c482684bb5b942cc5ce8d5388
NoorA23 <NoorA23@FLMS-X015893>
2018-10-24 13:26:19 -0400

    Added Autofill/fixed values 1-4/added sub (RefreshTextBox(f))

--- StudentsProjectAttempt1/Form1.Designer.vb
+++ StudentsProjectAttempt1/Form1.Designer.vb
@@ -34,54 +34,60 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(12, 377)
+        Me.TextBox1.Location = New System.Drawing.Point(9, 306)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(758, 22)
+        Me.TextBox1.Size = New System.Drawing.Size(570, 20)
         Me.TextBox1.TabIndex = 0
         '
         'Command_Btn
         '
-        Me.Command_Btn.Location = New System.Drawing.Point(663, 409)
+        Me.Command_Btn.Location = New System.Drawing.Point(497, 332)
+        Me.Command_Btn.Margin = New System.Windows.Forms.Padding(2)
         Me.Command_Btn.Name = "Command_Btn"
-        Me.Command_Btn.Size = New System.Drawing.Size(107, 31)
+        Me.Command_Btn.Size = New System.Drawing.Size(80, 25)
         Me.Command_Btn.TabIndex = 1
         Me.Command_Btn.Text = "Command"
         Me.Command_Btn.UseVisualStyleBackColor = True
         '
         'NumericUpDown1
         '
-        Me.NumericUpDown1.Location = New System.Drawing.Point(13, 413)
+        Me.NumericUpDown1.Location = New System.Drawing.Point(10, 336)
+        Me.NumericUpDown1.Margin = New System.Windows.Forms.Padding(2)
         Me.NumericUpDown1.Name = "NumericUpDown1"
-        Me.NumericUpDown1.Size = New System.Drawing.Size(53, 22)
+        Me.NumericUpDown1.Size = New System.Drawing.Size(40, 20)
         Me.NumericUpDown1.TabIndex = 2
         '
         'Label1
         '
-        Me.Label1.Location = New System.Drawing.Point(72, 413)
+        Me.Label1.Location = New System.Drawing.Point(54, 336)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(573, 23)
+        Me.Label1.Size = New System.Drawing.Size(430, 19)
         Me.Label1.TabIndex = 3
         '
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(350, 150)
+        Me.PictureBox1.Location = New System.Drawing.Point(262, 122)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(60, 60)
+        Me.PictureBox1.Size = New System.Drawing.Size(50, 50)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 4
         Me.PictureBox1.TabStop = False
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(782, 450)
+        Me.ClientSize = New System.Drawing.Size(586, 366)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.NumericUpDown1)
         Me.Controls.Add(Me.Command_Btn)
         Me.Controls.Add(Me.TextBox1)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
--- StudentsProjectAttempt1/Form1.vb
+++ StudentsProjectAttempt1/Form1.vb
@@ -1,31 +1,39 @@
 ﻿Public Class Form1
-
+    Sub RefreshTextBox(f)
+        TextBox1.Text = f
+    End Sub
     Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
         Dim Value = NumericUpDown1.Value
         If Value = 1 Then
-            Label1.Text = "MoveUp() - Moves the character one unit up."
-        ElseIf Value = 0 Then
-            Label1.Text = ""
+            Label1.Text = "MoveUp() - Moves the character a certain amount of units up."
+            RefreshTextBox("MoveUp()")
         ElseIf Value = 2 Then
-            Label1.Text = "MoveDown() - Moves the character one unit down."
+            Label1.Text = "MoveDown() - Moves the character a certain amount of units down."
+            RefreshTextBox("MoveDown()")
         ElseIf Value = 3 Then
-            Label1.Text = "MoveLeft() - Moves the character one unit left."
+            Label1.Text = "MoveLeft() - Moves the character a certain amount of units left."
+            RefreshTextBox("MoveLeft()")
         ElseIf Value = 4 Then
-            Label1.Text = "MoveRight() - Moves the character one unit right."
+            Label1.Text = "MoveRight() - Moves the character a certain amount of units right."
+            RefreshTextBox("MoveRight()")
+        Else
+            Label1.Text = ""
+            RefreshTextBox("")
         End If
     End Sub
 
     Private Sub Command_Btn_Click(sender As Object, e As EventArgs) Handles Command_Btn.Click
 
         If TextBox1.Text = "MoveUp()" Then
-
+            PictureBox1.Top -= 10
         ElseIf TextBox1.Text = "MoveDown()" Then
-
+            PictureBox1.Top += 10
         ElseIf TextBox1.Text = "MoveLeft()" Then
-
+            PictureBox1.Left -= 10
         ElseIf TextBox1.Text = "MoveRight()" Then
-
+            PictureBox1.Left += 10
 
         End If
     End Sub
+
 End Class
