2ebd7590009ea0c54968754909cfd83088155d76
Ataul <ataulnoor75@gmail.com>
2018-10-29 23:36:43 -0400

    Completely finished MixedMaker tab.

--- StudentsProjectAttempt1/Form1.Designer.vb
+++ StudentsProjectAttempt1/Form1.Designer.vb
@@ -38,11 +38,16 @@ Partial Class Form1
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
         Me.TabPage2 = New System.Windows.Forms.TabPage()
-        Me.ListBox2 = New System.Windows.Forms.ListBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
         CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Character, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -179,6 +184,11 @@ Partial Class Form1
         '
         'TabPage2
         '
+        Me.TabPage2.Controls.Add(Me.TextBox4)
+        Me.TabPage2.Controls.Add(Me.Button3)
+        Me.TabPage2.Controls.Add(Me.Button4)
+        Me.TabPage2.Controls.Add(Me.TextBox5)
+        Me.TabPage2.Controls.Add(Me.ListBox3)
         Me.TabPage2.Controls.Add(Me.TextBox3)
         Me.TabPage2.Controls.Add(Me.Button2)
         Me.TabPage2.Controls.Add(Me.Button1)
@@ -192,46 +202,87 @@ Partial Class Form1
         Me.TabPage2.Text = "Mixed Maker"
         Me.TabPage2.UseVisualStyleBackColor = True
         '
-        'ListBox2
+        'TextBox4
         '
-        Me.ListBox2.FormattingEnabled = True
-        Me.ListBox2.ItemHeight = 16
-        Me.ListBox2.Location = New System.Drawing.Point(31, 86)
-        Me.ListBox2.Name = "ListBox2"
-        Me.ListBox2.Size = New System.Drawing.Size(159, 260)
-        Me.ListBox2.TabIndex = 0
+        Me.TextBox4.Location = New System.Drawing.Point(204, 391)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(156, 22)
+        Me.TextBox4.TabIndex = 9
         '
-        'TextBox2
+        'Button3
         '
-        Me.TextBox2.Location = New System.Drawing.Point(31, 16)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(159, 22)
-        Me.TextBox2.TabIndex = 1
+        Me.Button3.Location = New System.Drawing.Point(204, 356)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(158, 29)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Clear"
+        Me.Button3.UseVisualStyleBackColor = True
         '
-        'Button1
+        'Button4
         '
-        Me.Button1.Location = New System.Drawing.Point(31, 48)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(158, 29)
-        Me.Button1.TabIndex = 2
-        Me.Button1.Text = "Add"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button4.Location = New System.Drawing.Point(204, 52)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(158, 29)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "Add"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(204, 20)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(159, 22)
+        Me.TextBox5.TabIndex = 6
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.ItemHeight = 16
+        Me.ListBox3.Location = New System.Drawing.Point(204, 90)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(159, 260)
+        Me.ListBox3.TabIndex = 5
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(26, 391)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(156, 22)
+        Me.TextBox3.TabIndex = 4
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(31, 352)
+        Me.Button2.Location = New System.Drawing.Point(26, 356)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(158, 29)
         Me.Button2.TabIndex = 3
         Me.Button2.Text = "Clear"
         Me.Button2.UseVisualStyleBackColor = True
         '
-        'TextBox3
+        'Button1
         '
-        Me.TextBox3.Location = New System.Drawing.Point(31, 387)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(156, 22)
-        Me.TextBox3.TabIndex = 4
+        Me.Button1.Location = New System.Drawing.Point(26, 52)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(158, 29)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(26, 20)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(159, 22)
+        Me.TextBox2.TabIndex = 1
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 16
+        Me.ListBox2.Location = New System.Drawing.Point(26, 90)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(159, 260)
+        Me.ListBox2.TabIndex = 0
         '
         'Form1
         '
@@ -273,4 +324,9 @@ Partial Class Form1
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents ListBox2 As ListBox
     Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents ListBox3 As ListBox
 End Class
--- StudentsProjectAttempt1/Form1.vb
+++ StudentsProjectAttempt1/Form1.vb
@@ -7,8 +7,10 @@
     Dim MoveLeftUsed = False
     Dim MoveRightUsed = False
     Dim Mission1Enabled = True
+    Dim Mission1Done = False
 
     Dim NameofValue5 = ""
+    Dim NameofValue6 = ""
 
     Sub RefreshCharacterLocation()
         CharacterX = Character.Location.X
@@ -37,6 +39,9 @@
         ElseIf Not (ListBox2.Items.ToString() = "") And Value = 5 Then
             Label1.Text = NameofValue5
             RefreshTextBox(NameofValue5)
+        ElseIf Not (ListBox3.Items.ToString() = "") And Value = 6 Then
+            Label1.Text = NameofValue6
+            RefreshTextBox(NameofValue6)
         Else
             Label1.Text = ""
             RefreshTextBox("")
@@ -73,22 +78,18 @@
                 ListBox2.SelectedIndex = Numberfor5
                 If ListBox2.SelectedItem = "MoveUp()" Then
                     Character.Top -= 10
-                    ListBox1.Items.Add("MoveUp()")
                     RefreshCharacterLocation()
                     MoveUpUsed = True
                 ElseIf ListBox2.SelectedItem = "MoveDown()" Then
                     Character.Top += 10
-                    ListBox1.Items.Add("MoveDown()")
                     RefreshCharacterLocation()
                     MoveDownUsed = True
                 ElseIf ListBox2.SelectedItem = "MoveLeft()" Then
                     Character.Left -= 10
-                    ListBox1.Items.Add("MoveLeft()")
                     RefreshCharacterLocation()
                     MoveLeftUsed = True
                 ElseIf ListBox2.SelectedItem = "MoveRight()" Then
                     Character.Left += 10
-                    ListBox1.Items.Add("MoveRight()")
                     RefreshCharacterLocation()
                     MoveRightUsed = True
                 ElseIf Numberfor5 <= Limitfor5 Then
@@ -96,11 +97,41 @@
                 End If
                 Numberfor5 += 1
             End While
+            ListBox1.Items.Add(NameofValue5)
+        ElseIf TextBox1.Text = NameofValue6 And Not (NameofValue6 = "") And Not (ListBox3.Items.ToString() = "") Then
+            Dim Numberfor6 = 0
+            Dim Limitfor6 = ListBox3.Items.Count - 1
+            ListBox3.SelectedIndex = Numberfor6
+            While Numberfor6 <= Limitfor6
+                ListBox3.SelectedIndex = Numberfor6
+                If ListBox3.SelectedItem = "MoveUp()" Then
+                    Character.Top -= 10
+                    RefreshCharacterLocation()
+                    MoveUpUsed = True
+                ElseIf ListBox3.SelectedItem = "MoveDown()" Then
+                    Character.Top += 10
+                    RefreshCharacterLocation()
+                    MoveDownUsed = True
+                ElseIf ListBox3.SelectedItem = "MoveLeft()" Then
+                    Character.Left -= 10
+                    RefreshCharacterLocation()
+                    MoveLeftUsed = True
+                ElseIf ListBox3.SelectedItem = "MoveRight()" Then
+                    Character.Left += 10
+                    RefreshCharacterLocation()
+                    MoveRightUsed = True
+                ElseIf Numberfor6 <= Limitfor6 Then
+                    Exit While
+                End If
+                Numberfor6 += 1
+            End While
+            ListBox1.Items.Add(NameofValue6)
         End If
 
-        If MoveRightUsed = True And MoveLeftUsed = True And MoveUpUsed = True And MoveDownUsed = True And Mission1Enabled = True Then
+        If MoveRightUsed = True And MoveLeftUsed = True And MoveUpUsed = True And MoveDownUsed = True And Mission1Enabled = True And Mission1Done = False Then
             PictureBox1.Visible = True
             Mission1Check.Enabled = True
+            Mission1Done = True
             Label4.Text = "Mission 2 - "
         End If
     End Sub
@@ -115,7 +146,7 @@
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        ListBox1.Items.Clear()
+        ListBox1.SelectedItem.Clear()
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
@@ -124,5 +155,17 @@
 
     Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
         NameofValue5 = TextBox3.Text
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ListBox3.Items.Add(TextBox5.Text)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ListBox3.SelectedItem.Clear()
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+        NameofValue6 = TextBox4.Text
     End Sub
 End Class
